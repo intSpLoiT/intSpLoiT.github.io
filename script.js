0765e2ea6cce286c111265bf916bb1fc7182e08a
intSpLoiT Framework Project <167871296+intSpLoiT@users.noreply.github.com>
2025-03-02 09:11:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const matrix = document.getElementById("matrix");
-    const consoleDiv = document.getElementById("console");
-    const typingDiv = document.getElementById("typing");
-
-    function startMatrix() {
-        let chars = "01";
-        let matrixText = "";
-        for (let i = 0; i < 500; i++) {
-            matrixText += chars[Math.floor(Math.random() * chars.length)];
-        }
-        matrix.innerHTML = `<span>${matrixText}</span>`;
-    }
-    let matrixInterval = setInterval(startMatrix, 100);
-
-    // 5 saniye sonra Matrix'i kapat ve Terminal'i başlat
+    // Matrix efekti 5 saniye sürecek
     setTimeout(() => {
-        clearInterval(matrixInterval);
-        matrix.style.display = "none";
-        consoleDiv.style.display = "block";
-
-        startTerminal();
+        $("#matrix").fadeOut(1000, function() {
+            $("#console").fadeIn(1000);
+            startTerminalEffect();
+        });
     }, 5000);
-
-    function startTerminal() {
-        new Typewriter(typingDiv, {
-            loop: false,
-            delay: 50,
-        })
-            .typeString("root@intSpLoiT:~# System booting up...<br>")
-            .pauseFor(500)
-            .typeString("root@intSpLoiT:~# Checking security protocols...<br>")
-            .pauseFor(500)
-            .typeString("root@intSpLoiT:~# Loading intSpLoiT modules...<br>")
-            .pauseFor(500)
-            .typeString("root@intSpLoiT:~# Verifying user identity...<br>")
-            .pauseFor(500)
-            .typeString("root@intSpLoiT:~# Access granted!<br>")
-            .pauseFor(500)
-            .typeString("root@intSpLoiT:~# Welcome, intSpLoiT user.<br>")
-            .pauseFor(500)
-            .typeString("root@intSpLoiT:~# Fetching latest exploits...<br>")
-            .pauseFor(500)
-            .typeString("root@intSpLoiT:~# Connection to GitHub established!<br>")
-            .pauseFor(500)
-            .typeString(
-                'root@intSpLoiT:~# GitHub: <a href="https://github.com/intSpLoiT" target="_blank">https://github.com/intSpLoiT</a><br>'
-            )
-            .pauseFor(500)
-            .typeString(
-                'root@intSpLoiT:~# Blog: <a href="#" onclick="alert(\'Hazırlanıyor...\')">Hazırlanıyor...</a><br>'
-            )
-            .pauseFor(500)
-            .typeString("root@intSpLoiT:~# Terminal ready.<br>")
-            .start();
-    }
 });
+
+function startTerminalEffect() {
+    let typewriter = new Typewriter(document.getElementById("typing"), {
+        loop: false,
+        delay: 40
+    });
+
+    typewriter
+        .typeString('root@intSpLoiT:~# Initializing system...')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('root@intSpLoiT:~# Fetching resources...')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('root@intSpLoiT:~# Scanning network...')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('root@intSpLoiT:~# Exploit modules loaded...')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('root@intSpLoiT:~# Checking vulnerabilities...')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('root@intSpLoiT:~# Welcome to intSpLoiT Terminal')
+        .pauseFor(1000)
+        .start();
+}
